cb6c71b18aed9cd8286a33deeea0993d9e741238
Michael Callaro <michael.callaro@hcahealthcare.com>
2018-04-13 14:49:13 -0500

    Removed the first time check on the bulk import

--- js/pageload.js
+++ js/pageload.js
@@ -152,13 +152,11 @@ window.addEventListener("load",function() {
 			attendee_email = $("<div class=\"form-group col-sm-3\"><label for=\"email\">Email</label><input type=\"text\" class=\"form-control\" id=\"quick-sign-in-name\" name=\"email\" autocomplete=\"on\" placeholder=\"Enter an email\"></div>"),
 			attendee_first_name = $("<div class=\"form-group col-sm-3\"><label for=\"firstName\">First Name</label><input type=\"text\" class=\"form-control\" id=\"firstName\" name=\"firstName\" autocomplete=\"on\" placeholder=\"First Name\"></div>"),
 			attendee_last_name = $("<div class=\"form-group col-sm-3\"><label for=\"lastName\">Last Name</label><input type=\"text\" class=\"form-control\" id=\"lastName\" name=\"lastName\" autocomplete=\"on\" placeholder=\"Last Name\"></div>"),
-			attendee_first_time = $("<div class=\"form-group col-sm-2\"><div class=\"checkbox\"><label><input type=\"checkbox\" name=\"first-time\"> First Time</label></div></div>")
 			attendee_removal = $("<div class=\"pull-left\"><button type=\"button\" class=\"remove btn btn-default active\"><i class='glyphicon glyphicon-minus' aria-hidden=\"true\"></i><span class=\"sr-only\">Remove a field</span></button></div>");
 
 		attendee_field_wrapper.append(attendee_email);
 		attendee_field_wrapper.append(attendee_first_name);
 		attendee_field_wrapper.append(attendee_last_name);
-		attendee_field_wrapper.append(attendee_first_time);
 		attendee_field_wrapper.append(attendee_removal);
 		attendee_wrapper.append(attendee_field_wrapper);
 		// Set the focus
