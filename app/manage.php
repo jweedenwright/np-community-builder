bf11cf563a9ee779f8100270de86e08ec7a145d4
Jeremiah WW <jeremiah.weedenwright@hcahealthcare.com>
2018-04-13 10:43:20 -0500

    First Update Volunteer function working

--- app/manage.php
+++ app/manage.php
@@ -20,9 +20,59 @@
 
 ////////////////////////////////////////////////////				
 // MANAGE VOLUNTEER
+// - TEST: type=volunteer-period&vol-id=1&signintime=03/15/2018 1:30 PM&signouttime=03/15/2018 3:30 PM&location=7&task=1&organization=HCA
 
 			if ($manage_type == "volunteer") {
+				echo "VOLUNTEER!";
 				// Make sure we have required values for a VOLUNTEER update
+				if(!isset($_POST['fn'])) {
+					$return_message = "Must provide first name for volunteer.";
+				} elseif(!isset($_POST['ln'])) {
+					$return_message = "Must provide last name for volunteer.";
+				} elseif(!isset($_POST['email'])) {
+					$return_message = "Must provide email for volunteer.";
+				} else				
+					// Sanitize Strings
+					$vol_id = filter_var ( $_POST['vol-id'], FILTER_SANITIZE_STRING);
+					$vol_fn = filter_var ( $_POST['fn'], FILTER_SANITIZE_STRING);
+					$vol_ln = filter_var ( $_POST['ln'], FILTER_SANITIZE_STRING);
+					$vol_email = filter_var ( $_POST['email'], FILTER_SANITIZE_STRING);
+					
+					// Update String Query
+					$update_string = "UPDATE volunteer
+										SET email = '".$vol_email."'
+											,first_name = '".$vol_fn."'
+											,last_name = '".$vol_ln."'";
+					
+					// Optional fields
+					 if(isset($_POST['phone'])) {
+						 $vol_phone = filter_var ( $_POST['phone'], FILTER_SANITIZE_STRING);
+						 $update_string = $update_string . ",emergency_contact_phone = '".$vol_phone."'";
+	   				}
+					if(isset($_POST['skills'])) {
+						$vol_skills = filter_var ( $_POST['skills'], FILTER_SANITIZE_STRING);
+						 $update_string = $update_string . ",skills = '".$vol_skills."'";
+	   				}
+					if(isset($_POST['interests'])) {
+						$vol_interests = filter_var ( $_POST['interests'], FILTER_SANITIZE_STRING);
+						 $update_string = $update_string . ",interests = '".$vol_interests."'";
+	   				}
+					if(isset($_POST['availability'])) {
+						$vol_availability = filter_var ( $_POST['availability'], FILTER_SANITIZE_STRING);
+						 $update_string = $update_string . ",availability = '".$vol_availability."'";
+	   				}
+					if(isset($_POST['email_dist'])) {
+						$vol_email_dist = filter_var ( $_POST['email_dist'], FILTER_SANITIZE_STRING);
+						 $update_string = $update_string . ",include_email_dist = '".$vol_email_dist."'";
+	   				} 
+					$update_string = $update_string . "WHERE id = ".$vol_id;
+					if ($db->executeStatement($update_string,[])) {
+						// Success
+						$return_message = "Successfully Updated Volunteer!";
+					} else {
+						// Failure
+						$return_message = "Sorry! Was unable to update the volunteer.";
+					}
 
 ////////////////////////////////////////////////////				
 // MANAGE VOLUNTEER PERIOD
@@ -89,12 +139,16 @@
 		} else {
 			$return_message = "Sorry! There was an issue with the action/type you attempted to take.";
 		}
-
 		if (strrpos($referring_url, "?")) {
-			header("Location: " . $referring_url . "&message=".$return_message);	
+			if (strrpos($referring_url, "&message=")) {
+				// Message already in alert, remove and replace
+				$referring_url = preg_replace('/message=.*/', "message=".$return_message, $referring_url);
+				header("Location: " . $referring_url);
+			} else {
+				header("Location: " . $referring_url . "&message=".$return_message);	
+			}
 		} else {
 			header("Location: " . $referring_url . "?message=".$return_message);			
 		}
-
 	}
 ?>
