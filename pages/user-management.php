17ba34417696992d472dfa2dad1e9f763b2afde1
Michael Callaro <michael.callaro@hcahealthcare.com>
2018-04-12 21:48:05 -0500

    Added in edit btns to the table and added styles for the btns

--- pages/user-management.php
+++ pages/user-management.php
@@ -34,7 +34,7 @@
 			<table class="table table-striped">
 				<thead>
 					<tr>
-					    <th>ID</th>
+						<th>ID</th>
 						<th>First Name</th>
 						<th>Last Name</th>
 						<th>Email</th>
@@ -65,12 +65,10 @@
 				<!--<h3>Can use the variable edit_mode (pass the value 'edit' in URL as a parameter) as a flag between edit mode and not edit mode</h3>-->
 				<!--<h4>Edit Mode? <?=$edit_mode?></h4>-->
 				<form id="volunteer-management-form">
-					<h1><font color ="#00008B"><?=$volunteer["first_name"]?>&nbsp;<?=$volunteer["last_name"]?></font></h1>
 					<div class="pull-right">
-					<a href="#" class="edit-period" data-toggle="modal" data-target="#edit-modal" onclick="editDetails(this); return false;">
-	                <i class="glyphicon glyphicon-pencil" aria-hidden="true"></i>
-	                <span>Edit</span></a>
+						<a href="#" class="edit-details-btn" onclick="editDetails(); return false;">Edit</a>
 					</div>
+					<h1><font color ="#00008B"><?=$volunteer["first_name"]?>&nbsp;<?=$volunteer["last_name"]?></font></h1>
 					<h4><?=$volunteer["email"]?></h4>
 					<h4><?=$volunteer["emergency_contact_phone"]?></h4>
 					<?php
@@ -93,25 +91,26 @@
 				<table class="table table-striped">
 				<thead>
 					<tr>
-					    <th>ID</th>
+						<th>ID</th>
 						<th>Date</th>
 						<th>Time</th>
 						<th>Duration</th>
 						<th>Activity</th>
 						<th>Location</th>
 						<th>Affiliation</th>
+						<th></th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php
 						if (sizeof($vol_periods) > 0) {
-							?><ul><?php
+							?>
+							<?php
 							foreach ($vol_periods as $vol_period) {
 								// QUERY JOB AND LOCATION
-						
 							?>
 						<tr>
-						    <td><?=$vol_period["id"]?></td>
+							<td><?=$vol_period["id"]?></td>
 							<?php
 							$checkin_date = date_parse_from_format ( $sql_date_format , $vol_period["check_in_time"]);
 							$checkedin_date = $checkin_date["month"] . "-" . $checkin_date["day"] . "-" . $checkin_date["year"]; 
@@ -137,22 +136,88 @@
 								<td><?=$location_row["location_name"]?></td>
 								<?php
 							}
-					}				
+					}
 					?>
 						<td><?=$vol_period["affiliation"]?></td>
+						<td>
+							<a href="#" class="edit-period" data-id="<?=$vol_period["id"]?>" data-signin="<?=$vol_period["check_in_time"]?>" data-signout="<?=$vol_period["check_in_time"]?>" data-activity="<?=$job_type_row["job_type"]?>" data-location="<?=$location_row["location_name"]?>" data-org="<?=$vol_period["affiliation"]?>" data-toggle="modal" data-target="#edit-modal" onclick="editPeriod(this); return false;">
+								<i class="glyphicon glyphicon-pencil" aria-hidden="true"></i>
+								<span class="sr-only">Edit</span>
+							</a>
+						</td>
 						</tr>
 							<?php
 							}
-							?></ul><?php
+							?>
+							<?php
 						}
 					?>
 				</tbody>
 				</table>
 				</div>
 				</form>
+
+				<div class="modal fade" id="edit-modal" tabindex="-1" role="dialog" aria-labelledby="edit-label">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<h4 class="modal-title" id="edit-label">Edit Period</h4>
+							</div>
+							<div class="modal-body">
+								<form id="edit-period-form">
+									<input type="hidden" id="vol-id" name="vol-id" value="">
+
+									<div class="form-group">
+										<label for="datetime-picker">Sign In</label>
+										<input type='text' class="form-control datetime-picker" id="signin-datetime-picker" data-format="yyyy-MM-dd hh:mm:00" name="signintime" placeholder="MM/DD/YYYY 12:01 AM" />
+									</div>
+									<div class="form-group">
+										<label for="datetime-picker">Sign Out</label>
+										<input type='text' class="form-control datetime-picker" id="signout-datetime-picker" data-format="yyyy-MM-dd hh:mm:00" name="signouttime" placeholder="MM/DD/YYYY 12:01 AM" />
+									</div>
+									<div class="form-group">
+										<label for="activity">Activity</label>
+										<select required class="form-control" id="task" name="task">
+											<option disabled selected="true" value="">Please Select A Task</option>
+											<?php
+												foreach ($type_results as $row) {
+													?>
+														<option value="<?=$row['id']?>"><?=$row['job_type']?></option>
+													<?php
+												}
+											?>
+										</select>
+									</div>
+									<div class="form-group">
+										<label for="location">Location</label>
+										<select required class="form-control" id="location" name="location">
+											<option disabled selected="true" value="">Please Choose A Location</option>
+											<?php
+												foreach ($location_results as $row) {
+													?>
+														<option value="<?=$row['id']?>"><?=$row['location_name']?></option>
+													<?php
+												}
+											?>
+										</select>
+									</div>
+									<div class="form-group">
+										<label for="organization">Affiliation</label>
+										<input class="form-control" type="text" id="organization" name="organization" value="">
+									</div>
+								</form>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								<button type="button" class="btn btn-success">Save changes</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			<?php
 		}
 	}
 	include_once '../footer.php';
 ?>
-	
