420741e90724f350f3854a2b7ebe34dc5f9aa2b1
Weeden-wright Jeremiah <OCD9105@HCA.corpad.net>
2020-02-20 21:48:32 -0600

    Updates to database to support bgcrc

--- future_db/table_maintenance.sql
+++ future_db/table_maintenance.sql
@@ -1,13 +1,13 @@
+-- BGCRC Database
 -- Drop Tables to Start
 IF OBJECT_ID('dbo.volunteer_period', 'U') IS NOT NULL DROP TABLE dbo.volunteer_period
 IF OBJECT_ID('dbo.feedback', 'U') IS NOT NULL DROP TABLE dbo.feedback
 IF OBJECT_ID('dbo.volunteer', 'U') IS NOT NULL DROP TABLE dbo.volunteer
-IF OBJECT_ID('dbo.metric', 'U') IS NOT NULL DROP TABLE dbo.metric
+IF OBJECT_ID('dbo.event', 'U') IS NOT NULL DROP TABLE dbo.event
 IF OBJECT_ID('dbo.app_user', 'U') IS NOT NULL DROP TABLE dbo.app_user
-IF OBJECT_ID('dbo.program', 'U') IS NOT NULL DROP TABLE dbo.program
-IF OBJECT_ID('dbo.metric_category', 'U') IS NOT NULL DROP TABLE dbo.metric_category
-IF OBJECT_ID('dbo.metric_name', 'U') IS NOT NULL DROP TABLE dbo.metric_name
-IF OBJECT_ID('dbo.measure_type', 'U') IS NOT NULL DROP TABLE dbo.measure_type
+IF OBJECT_ID('dbo.user_type', 'U') IS NOT NULL DROP TABLE dbo.user_type
+IF OBJECT_ID('dbo.address', 'U') IS NOT NULL DROP TABLE dbo.address
+IF OBJECT_ID('dbo.emergency_contact', 'U') IS NOT NULL DROP TABLE dbo.emergency_contact
 IF OBJECT_ID('dbo.job_type', 'U') IS NOT NULL DROP TABLE dbo.job_type
 IF OBJECT_ID('dbo.location', 'U') IS NOT NULL DROP TABLE dbo.location
 
@@ -23,26 +23,65 @@ CREATE UNIQUE INDEX UQ_job_type ON dbo.job_type (job_type)
 CREATE TABLE dbo.location (
 	id int NOT NULL IDENTITY(1,1),
 	location_name varchar(100) NOT NULL,
-	delivery BIT NOT NULL DEFAULT(0),
-	internal BIT NOT NULL DEFAULT(0),
 	active bit NOT NULL DEFAULT(1),
 	CONSTRAINT PK_location PRIMARY KEY (id)
 )
 CREATE UNIQUE INDEX UQ_location_name ON dbo.location (location_name)
 
-CREATE TABLE dbo.volunteer (
+CREATE TABLE dbo.event (
+	id int NOT NULL IDENTITY(1,1),
+	event_name varchar(100) NOT NULL,
+	event_date datetime NOT NULL,
+	CONSTRAINT PK_event PRIMARY KEY (id)
+)
+CREATE UNIQUE INDEX UQ_event ON dbo.event (event_name)
+
+CREATE TABLE dbo.user_type (
+	id int NOT NULL IDENTITY(1,1),
+	user_type varchar(100) NOT NULL,
+	CONSTRAINT PK_user_type PRIMARY KEY (id)
+)
+CREATE UNIQUE INDEX UQ_user_type ON dbo.user_type (user_type)
+
+CREATE TABLE dbo.address (
+	id int NOT NULL IDENTITY(1,1),
+	street_one varchar(255) NOT NULL,
+	street_two varchar(255) NULL,
+	city varchar(100) NOT NULL,
+	state varchar(2) NOT NULL,
+	zip int NOT NULL,
+	user_type_name varchar(100) NOT NULL,
+	CONSTRAINT PK_address PRIMARY KEY (id)
+)
+
+CREATE TABLE dbo.emergency_contact (
 	id int NOT NULL IDENTITY(1,1),
 	first_name varchar(100) NOT NULL,
 	last_name varchar(100) NOT NULL,
+	phone varchar(15) NOT NULL,
+	CONSTRAINT PK_emergency_contact PRIMARY KEY (id)
+)
+
+CREATE TABLE dbo.volunteer (
+	id int NOT NULL IDENTITY(1,1),
+	first_name varchar(100) NOT NULL,
+	middle_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	suffix varchar(10) NOT NULL,
 	email varchar(255) NOT NULL,
+	phone varchar(15) NOT NULL,
+	dob datetime NOT NULL,
 	skills varchar(8000) NULL,
-	emergency_contact_phone varchar(45) NULL,
 	interests varchar(8000) NULL,
 	availability varchar(255) NULL,
 	find_out_about_us varchar(255) NULL,
 	include_email_dist bit NULL,
+	address_id int NOT NULL,
+	emergency_contact_id int NOT NULL,
 	active bit NOT NULL DEFAULT(1),
-	CONSTRAINT PK_volunteer PRIMARY KEY (id)
+	CONSTRAINT PK_volunteer PRIMARY KEY (id),
+	CONSTRAINT FK_volunteer_address FOREIGN KEY (address_id) REFERENCES dbo.address(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FK_volunteer_emergency_contact FOREIGN KEY (emergency_contact_id) REFERENCES dbo.emergency_contact(id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 CREATE UNIQUE INDEX UQ_volunteer_email ON dbo.volunteer (email)
 
@@ -66,6 +105,7 @@ CREATE TABLE dbo.volunteer_period (
 	first_time int NOT NULL,
 	job_type_id int NOT NULL,
 	location_id int NOT NULL,
+	event_id int NOT NULL,
 	community_service_hours int NULL DEFAULT (NULL),
 	volunteer_id int NOT NULL,
 	feedback_id int NULL,
@@ -73,43 +113,9 @@ CREATE TABLE dbo.volunteer_period (
 	CONSTRAINT FK_volunteer_period_jobtype FOREIGN KEY (job_type_id) REFERENCES dbo.job_type(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT FK_volunteer_period_location FOREIGN KEY (location_id) REFERENCES dbo.location(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT FK_volunteer_period_volunteer FOREIGN KEY (volunteer_id) REFERENCES dbo.volunteer(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT FK_volunteer_period_feedback FOREIGN KEY (feedback_id) REFERENCES dbo.feedback(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT FK_volunteer_period_feedback FOREIGN KEY (feedback_id) REFERENCES dbo.feedback(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FK_volunteer_period_event FOREIGN KEY (event_id) REFERENCES dbo.event(id) ON DELETE CASCADE ON UPDATE CASCADE
 )
-
--- NEW TABLES
-CREATE TABLE dbo.metric_category (
-	id int NOT NULL IDENTITY(1,1),
-	metric_category varchar(100) NOT NULL,
-	CONSTRAINT PK_metric_category PRIMARY KEY (id)
-)
-CREATE UNIQUE INDEX UQ_metric_category ON dbo.metric_category (metric_category)
-
-CREATE TABLE dbo.measure_type (
-	id int NOT NULL IDENTITY(1,1),
-	measure_type varchar(100) NOT NULL,
-	measure_data_type varchar(50) NOT NULL,
-	CONSTRAINT PK_measure_type PRIMARY KEY (id)
-)
-CREATE UNIQUE INDEX UQ_measure_type ON dbo.measure_type (measure_type)
-
-CREATE TABLE dbo.metric (
-	id int NOT NULL IDENTITY(1,1),
-	metric_name varchar(100) NOT NULL,
-	metric_category_id int NOT NULL,
-	measure_type_id int NOT NULL,
-	CONSTRAINT FK_metric_metric_category FOREIGN KEY (metric_category_id) REFERENCES dbo.metric_category(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT FK_metric_measure_type FOREIGN KEY (measure_type_id) REFERENCES dbo.measure_type(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT PK_metric PRIMARY KEY (id)
-)
-CREATE UNIQUE INDEX UQ_metric ON dbo.metric (metric_name)
-
-CREATE TABLE dbo.program (
-	id int NOT NULL IDENTITY(1,1),
-	program varchar(100) NOT NULL,
-	active bit NOT NULL DEFAULT(1),
-	CONSTRAINT PK_program PRIMARY KEY (id)
-)
-CREATE UNIQUE INDEX UQ_program ON dbo.program (program)
 
 -- USER TABLE
 CREATE TABLE dbo.app_user (
@@ -118,110 +124,50 @@ CREATE TABLE dbo.app_user (
 	password varchar(200) NOT NULL,
 	date_added datetime NOT NULL,
 	reset_id varchar(200) NULL,
-	is_admin bit NOT NULL,
+	user_type_id int NOT NULL,
+	volunteer_id int NOT NULL,
 	active bit NOT NULL DEFAULT(1),
-	CONSTRAINT PK_app_user PRIMARY KEY(id)
+	CONSTRAINT PK_app_user PRIMARY KEY(id),
+	CONSTRAINT FK_app_user_user_type FOREIGN KEY (user_type_id) REFERENCES dbo.user_type(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FK_app_user_volunteer FOREIGN KEY (volunteer_id) REFERENCES dbo.volunteer(id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 CREATE UNIQUE INDEX UQ_users ON dbo.app_user (username)
 
--- NEW METRIC ENTRY TABLE TO HANDLE LOGGED METRICS
-CREATE TABLE dbo.metric_entry (
-	id int NOT NULL IDENTITY(1,1),
-	timestamp datetime NOT NULL,
-	metric_id int NOT NULL,
-	location_id int NULL,
-	program_id int NULL,
-	app_user_id int NOT NULL,
-	value varchar(200) NOT NULL,
-	CONSTRAINT PK_metric_entry PRIMARY KEY (id),
-	CONSTRAINT FK_metric_entry_metric FOREIGN KEY (metric_id) REFERENCES dbo.metric(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT FK_metric_entry_location FOREIGN KEY (location_id) REFERENCES dbo.location(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT FK_metric_entry_program FOREIGN KEY (program_id) REFERENCES dbo.program(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT FK_metric_entry_app_user FOREIGN KEY (app_user_id) REFERENCES dbo.app_user(id) ON DELETE CASCADE ON UPDATE CASCADE
-) 
+
+
+
+
+
+
 
 -- DATA IMPORT
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('jeremiah.weedenwright@gmail.com','tester', '2016-12-05',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('mcallaro88@gmail.com','tester', '2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('divya.guntu@hcahealthcare.com','tester', '2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('teri@thenashvillefoodproject.org','tester','2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('munderwood@c3-consult.com','tester','2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('justin.threlkeld@gmail.com','tester','2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('joyce.pfeffer@hcahealthcare.com','tester','2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('mariah@thenashvillefoodproject.org','tester','2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('malinda@thenashvillefoodproject.org','tester','2018-04-12',1, 1);
-INSERT INTO dbo.app_user (username, password, date_added, is_admin, active) VALUES ('Jasmyn@thenashvillefoodproject.org','tester','2018-06-18',1, 1);
-
 SET IDENTITY_INSERT dbo.location ON
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (1,'Blackman Road Garden',0,0,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (2,'Haywood Lane Garden',0,0,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (5,'McGruder Garden',0,0,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (4,'South Hall Kitchen',0,0,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (6,'St Luke''s Kitchen',0,0,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (7,'Wedgewood Garden',0,0,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (3,'Woodmont Garden',0,0,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (9,'Fall Hamilton',0,1,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (10,'Cottage Cove',0,1,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (11,'Harvest Hands',0,1,1);
-INSERT INTO dbo.location (id,location_name,delivery,internal,active) VALUES (12,'Wedgewood Garden Neighbors',0,1,1);
+INSERT INTO dbo.location (id,location_name,active) VALUES (1,'Murfreesboro',1);
+INSERT INTO dbo.location (id,location_name,active) VALUES (2,'Shelbyville',1);
+INSERT INTO dbo.location (id,location_name,active) VALUES (3,'Smyrna',1);
 SET IDENTITY_INSERT dbo.location OFF
 
 SET IDENTITY_INSERT dbo.job_type ON
-INSERT INTO dbo.job_type (id,job_type,active) VALUES (1,'Cook',1);
-INSERT INTO dbo.job_type (id,job_type,active) VALUES (3,'Delivery',1);
-INSERT INTO dbo.job_type (id,job_type,active) VALUES (4,'Garden',1);
-INSERT INTO dbo.job_type (id,job_type,active) VALUES (11,'Gleaning',1);
-INSERT INTO dbo.job_type (id,job_type,active) VALUES (2,'Meal Prep',1);
-INSERT INTO dbo.job_type (id,job_type,active) VALUES (5,'Other',1);
-INSERT INTO dbo.job_type (id,job_type,active) VALUES (21,'Web / IT',1);
+INSERT INTO dbo.job_type (id,job_type,active) VALUES (1,'General',1);
+INSERT INTO dbo.job_type (id,job_type,active) VALUES (2,'Web / IT',1);
 SET IDENTITY_INSERT dbo.job_type OFF
 
-SET IDENTITY_INSERT dbo.metric_category ON
-INSERT INTO dbo.metric_category (id,metric_category) VALUES (1,'Meals')
-INSERT INTO dbo.metric_category (id,metric_category) VALUES (2,'Garden')
-SET IDENTITY_INSERT dbo.metric_category OFF
+SET IDENTITY_INSERT dbo.user_type ON
+INSERT INTO dbo.user_type (id,user_type) VALUES (1,'admin');
+INSERT INTO dbo.user_type (id,user_type) VALUES (2,'staff');
+INSERT INTO dbo.user_type (id,user_type) VALUES (3,'intern');
+INSERT INTO dbo.user_type (id,user_type) VALUES (4,'volunteer');
+SET IDENTITY_INSERT dbo.user_type OFF
 
-SET IDENTITY_INSERT dbo.measure_type ON
-INSERT INTO dbo.measure_type (id,measure_type,measure_data_type) VALUES (1,'Harvested in Lbs','decimal')
-INSERT INTO dbo.measure_type (id,measure_type,measure_data_type) VALUES (2,'Number','number')
-INSERT INTO dbo.measure_type (id,measure_type,measure_data_type) VALUES (3,'Currency','decimal')
-INSERT INTO dbo.measure_type (id,measure_type,measure_data_type) VALUES (4,'Servings','number')
-INSERT INTO dbo.measure_type (id,measure_type,measure_data_type) VALUES (5,'Donated in Lbs','decimal')
-INSERT INTO dbo.measure_type (id,measure_type,measure_data_type) VALUES (6,'Lbs','decimal')
-SET IDENTITY_INSERT dbo.measure_type OFF
+-- Setup Default volunteers
+INSERT INTO dbo.volunteer () VALUES();
 
-SET IDENTITY_INSERT dbo.metric ON
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (1,'Total Meals Shared',1,2)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (2,'Total Meals Reported',1,2)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (3,'Total Non-Program Meals',1,2)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (4,'Food Costs',1,3)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (5,'Local Farm Investment',1,3)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (6,'Meal Distribution Partners',1,2)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (7,'Program Participants - Meals',1,2)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (8,'Grocery Store',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (9,'Farm/Garden',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (10,'Restaurant',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (11,'Caterer',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (12,'Individual',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (13,'School',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (14,'Other - Food Donations',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (15,'Shared with Others',1,5)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (16,'Compost',1,6)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (17,'Fruit',2,1)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (18,'Greens',2,1)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (19,'Salad',2,1)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (20,'Herbs',2,1)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (21,'Eggs',2,1)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (22,'Other - Food Harvested',2,1)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (23,'Roots',2,1)
-INSERT INTO dbo.metric (id,metric_name,metric_category_id,measure_type_id) VALUES (24,'Program Participants - Gardens',2,2)
-SET IDENTITY_INSERT dbo.metric OFF
+-- Setup Default admins
+INSERT INTO dbo.app_user (username, password, date_added, user_type_id, volunteer_id, active) VALUES ('jeremiah.weedenwright@gmail.com','tester', '2020-02-20',1,1,1);
+INSERT INTO dbo.app_user (username, password, date_added, user_type_id, volunteer_id, active) VALUES ('','tester', '2020-02-20',1,2,1);
+INSERT INTO dbo.app_user (username, password, date_added, user_type_id, volunteer_id, active) VALUES ('Shalonda.Brown@bgcrc.net','tester', '2020-02-20',1,3,1);
+INSERT INTO dbo.app_user (username, password, date_added, user_type_id, volunteer_id, active) VALUES ('recruitment@bgcrc.net','tester','2020-02-20',1,4,1);
 
-SET IDENTITY_INSERT dbo.program ON
-INSERT INTO dbo.program (id,program,active) VALUES (1,'Nashville CARES',1)
-INSERT INTO dbo.program (id,program,active) VALUES (2,'Community Garden',1)
-INSERT INTO dbo.program (id,program,active) VALUES (3,'Market Gardens',1)
-INSERT INTO dbo.program (id,program,active) VALUES (4,'Garden Education',1)
-SET IDENTITY_INSERT dbo.program OFF
 
-SELECT * from dbo.metric
+
+SELECT * from dbo.location
