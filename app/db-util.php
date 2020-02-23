882150f618468292603bec9336136f8784bcb6fb
Weeden-wright Jeremiah <OCD9105@HCA.corpad.net>
2020-02-23 14:02:37 -0600

    Global code setup, working on individual pages and functionality to adapt to the new database now.

--- app/db-util.php
+++ app/db-util.php
@@ -1,4 +1,5 @@
 <?php
+
 //////////////////////
 // Class for handling DB Connections
 //////////////////////
@@ -8,33 +9,24 @@ class pdo_dblib_mssql {
 	private $childTrans = array();
 
 	public function __construct() {
+
+		$GLOBALS['current_protocol'] = $GLOBALS['protocol'];
+		$this->hostname = $GLOBALS['db_hostname'];
+		$this->dbname = $GLOBALS['db_name'];
+		$this->username = $GLOBALS['db_user'];
+		$this->port = $GLOBALS['db_port'];
+		$this->pwd = $GLOBALS['db_password'];
+		
+		// Get the domain to use as a variable
 		if($_SERVER['SERVER_NAME'] == $GLOBALS['prod_domain']) {
 			// Prod
 			$GLOBALS['current_domain'] = $GLOBALS['prod_domain'];
-			$GLOBALS['current_protocol'] = $GLOBALS['prod_protocol'];
-			$this->hostname = $GLOBALS['prod_db_hostname'];
-			$this->dbname = $GLOBALS['prod_db_name'];
-			$this->username = $GLOBALS['prod_db_user'];
-			$this->port = $GLOBALS['prod_db_port'];
-			$this->pwd = $GLOBALS['prod_db_password'];
 		} else if($_SERVER['SERVER_NAME'] == $GLOBALS['qa_domain']) {
 			// QA
 			$GLOBALS['current_domain'] = $GLOBALS['qa_domain'];
-			$GLOBALS['current_protocol'] = $GLOBALS['qa_protocol'];
-			$this->hostname = $GLOBALS['qa_db_hostname'];
-			$this->dbname = $GLOBALS['qa_db_name'];
-			$this->username = $GLOBALS['qa_db_user'];
-			$this->port = $GLOBALS['qa_db_port'];
-			$this->pwd = $GLOBALS['qa_db_password'];
 		} else {
 			// Dev (default)
 			$GLOBALS['current_domain'] = $GLOBALS['dev_domain'];
-			$GLOBALS['current_protocol'] = $GLOBALS['dev_protocol'];
-			$this->hostname = $GLOBALS['dev_db_hostname'];
-			$this->dbname = $GLOBALS['dev_db_name'];
-			$this->username = $GLOBALS['dev_db_user'];
-			$this->port = $GLOBALS['dev_db_port'];
-			$this->pwd = $GLOBALS['dev_db_password'];
 		}
 		$this->connect();
 	}
