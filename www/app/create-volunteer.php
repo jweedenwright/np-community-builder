3d643c1bee928dd06d8ca0769d1fa442080232b4
Kishan Patel <kpatel@genospace.com>
2020-10-30 14:31:47 -0400

    create-volunteer Auto stash before merge of "feature/create-volunteer" and "bgclub"

--- www/app/create-volunteer.php
+++ www/app/create-volunteer.php
@@ -31,164 +31,139 @@ if (!isLoggedIn()) {
         $return_message = "Must provide suffix for volunteer.";
     } elseif(!isset($_POST['phone'])) {
         $return_message = "Must provide phone for volunteer.";
-    } else
-    // Sanitize Strings
-    $vol_fn = filter_var ( $_POST['fn'], FILTER_SANITIZE_STRING);
-    $vol_ln = filter_var ( $_POST['ln'], FILTER_SANITIZE_STRING);
-    $vol_email = filter_var ( $_POST['email'], FILTER_SANITIZE_STRING);
-    $vol_phone = filter_var ( $_POST['phone'], FILTER_SANITIZE_STRING);
-    $vol_dob = filter_var($_POST['dob'], FILTER_SANITIZE_STRING);
-    $formatted_dob = date('Y-m-d', strtotime($vol_dob));
-    echo($formatted_dob);
-    $vol_dob_array = explode("/", $vol_dob);
-    $vol_dob_formatted = "${vol_dob_array[2]}-${vol_dob_array[0]}-${vol_dob_array[1]}";
-    $vol_dob = str_replace("/", "", $vol_dob);
-
-    $vol_middle_name = filter_var ( $_POST['middle_name'], FILTER_SANITIZE_STRING);
-    $vol_suffix = filter_var ( $_POST['suffix'], FILTER_SANITIZE_STRING);
-
-    // Update String Query
-    $insert_string = "INSERT INTO volunteer (email, first_name, last_name, dob, middle_name, suffix, phone)
-                      VALUES ('".$vol_email."','".$vol_fn."','".$vol_ln."','".$formatted_dob."','".$vol_middle_name."','".$vol_suffix."','".$vol_phone."')";
-
-    echo($insert_string);
-//    die();
-
-    $createFailed = false;
-    if ($db->executeStatement($insert_string,[])) {
-        // Success
-        echo('inserted successfully');
-        $return_message = "Successfully Created Volunteer!";
     } else {
-        // Failure
-        $return_message = "Sorry! Was unable to create the volunteer.";
-        $createFailed = true;
-    }
+        // Sanitize Strings
+        $vol_fn = filter_var($_POST['fn'], FILTER_SANITIZE_STRING);
+        $vol_ln = filter_var($_POST['ln'], FILTER_SANITIZE_STRING);
+        $vol_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+        $vol_phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
+        $vol_dob = filter_var($_POST['dob'], FILTER_SANITIZE_STRING);
+        if (isset($vol_dob)) {
+            $formatted_dob = date('Y-m-d', strtotime($vol_dob));
+        }
+        $vol_middle_name = filter_var($_POST['middle_name'], FILTER_SANITIZE_STRING);
+        $vol_suffix = filter_var($_POST['suffix'], FILTER_SANITIZE_STRING);
 
-    if (!$createFailed) {
-        // get volunteer id
-        $volunteer_id_query = "SELECT id FROM volunteer WHERE email = ?";
-        $results = $db->executeStatement($volunteer_id_query, array($vol_email))->fetchAll();
-        if (isset($results[0])) {
-            $vol_id = $results[0]['id'];
-        }
+        // insert Volunteer
+        $insert_volunteer_string = "INSERT INTO volunteer (email, first_name, last_name, dob, middle_name, suffix, phone)
+                          VALUES ('" . $vol_email . "','" . $vol_fn . "','" . $vol_ln . "','" . $formatted_dob . "','" . $vol_middle_name . "','" . $vol_suffix . "','" . $vol_phone . "')";
 
-        $volunteerUpdated = false;
-        $update_string = "UPDATE volunteer
-                            SET ";
-        // Optional fields
-//        if(isset($_POST['phone'])) {
-//            $vol_phone = filter_var ( $_POST['phone'], FILTER_SANITIZE_STRING);
-//            $update_string = $update_string . "emergency_contact_phone = '".$vol_phone."',";
-//            $volunteerUpdated = true;
-//        }
-        if(isset($_POST['skills'])) {
-            $vol_skills = filter_var ( $_POST['skills'], FILTER_SANITIZE_STRING);
-            $update_string = $update_string . "skills = '".$vol_skills."',";
-            $volunteerUpdated = true;
-        }
-        if(isset($_POST['interests'])) {
-            $vol_interests = filter_var ( $_POST['interests'], FILTER_SANITIZE_STRING);
-            $update_string = $update_string . "interests = '".$vol_interests."',";
-            $volunteerUpdated = true;
-        }
-        if(isset($_POST['availability'])) {
-            $availability = filter_var ( $_POST['availability'], FILTER_SANITIZE_STRING);
-            $update_string = $update_string . "availability = '".$availability."',";
-            $volunteerUpdated = true;
-        }
-        if(isset($_POST['find_out_about_us'])) {
-            $vol_find_out_about_us = filter_var ( $_POST['find_out_about_us'], FILTER_SANITIZE_STRING);
-            $update_string = $update_string . "find_out_about_us = '".$vol_find_out_about_us."',";
-            $volunteerUpdated = true;
-        }
-        if(isset($_POST['email_dist'])) {
-            $email_dist = filter_var ( $_POST['email_dist'], FILTER_SANITIZE_STRING);
-            $update_string = $update_string . "email_dist = '".$email_dist."',";
-            $volunteerUpdated = true;
-        }
-
-        $update_string = substr($update_string, 0, -1); // remove extra comma
-        $update_string = $update_string . "WHERE id = ".$vol_id;
-
-        // update address fields
-        $addressUpdated = false;
-        $update_address_string = "UPDATE address
-                                            SET ";
-        if(isset($_POST['street_one'])) {
-            $vol_street_one = filter_var ( $_POST['street_one'], FILTER_SANITIZE_STRING);
-            $update_address_string = $update_address_string . "street_one = '".$vol_street_one."',";
-            $addressUpdated = true;
-        }
-        if(isset($_POST['street_two'])) {
-            $vol_street_two = filter_var ( $_POST['street_two'], FILTER_SANITIZE_STRING);
-            $update_address_string = $update_address_string . "street_two = '".$vol_street_two."',";
-            $addressUpdated = true;
-        }
-        if(isset($_POST['city'])) {
-            $vol_city = filter_var ( $_POST['city'], FILTER_SANITIZE_STRING);
-            $update_address_string = $update_address_string . "city = '".$vol_city."',";
-            $addressUpdated = true;
-        }
-        if(isset($_POST['state'])) {
-            $vol_state = filter_var ( $_POST['state'], FILTER_SANITIZE_STRING);
-            $update_address_string = $update_address_string . "state = '".$vol_state."',";
-            $addressUpdated = true;
-        }
-        if(isset($_POST['zip'])) {
-            $vol_zip = filter_var ( $_POST['zip'], FILTER_SANITIZE_STRING);
-            $update_address_string = $update_address_string . "zip = '".$vol_zip."',";
-            $addressUpdated = true;
-        }
-        $update_address_string = substr($update_address_string, 0, -1); // remove extra comma
-        $update_address_string = $update_address_string . " WHERE volunteer_id = ".$vol_id;
-
-        // update emergency contact fields
-        $ecUpdated = false;
-        $update_ec_string = "UPDATE emergency_contact
-                                                SET ";
-        if(isset($_POST['ec_first_name'])) {
-            $ec_first_name = filter_var ( $_POST['ec_first_name'], FILTER_SANITIZE_STRING);
-            $update_ec_string = $update_ec_string . "first_name = '".$ec_first_name."',";
-            $ecUpdated = true;
-        }
-        if(isset($_POST['ec_last_name'])) {
-            $ec_last_name = filter_var ( $_POST['ec_last_name'], FILTER_SANITIZE_STRING);
-            $update_ec_string = $update_ec_string . "last_name = '".$ec_last_name."',";
-            $ecUpdated = true;
-        }
-        if(isset($_POST['ec_phone'])) {
-            $ec_phone = filter_var ( $_POST['ec_phone'], FILTER_SANITIZE_STRING);
-            $update_ec_string = $update_ec_string . "phone = '".$ec_phone."',";
-            $ecUpdated = true;
-        }
-        $update_ec_string = substr($update_ec_string, 0, -1); // remove extra comma
-        $update_ec_string = $update_ec_string . " WHERE volunteer_id = ".$vol_id;
-
-        // update Volunteer table
-        $updatedFailed = false;
-        if ($db->executeStatement($update_string,[])) {
-            if ($addressUpdated) {
-                // update Address table
-                if ($db->executeStatement($update_address_string,[])) {
-                    if ($ecUpdated) {
-                        // update Emergency Contact table
-                        if ($db->executeStatement($update_ec_string,[])) {
-                            $return_message = "Successfully Updated Volunteer!";
-                        } else {
-                            $updatedFailed = true;
-                        }
-                    }
-                } else {
-                    $updatedFailed = true;
-                }
-            }
+        $createFailed = false;
+        if ($db->executeStatement($insert_volunteer_string, [])) {
+            // Success
+            $return_message = "Successfully Created Volunteer!";
         } else {
-            $updatedFailed = true;
+            // Failure
+            $return_message = "Sorry! Was unable to create the volunteer.";
+            $createFailed = true;
         }
 
-        if ($updatedFailed) {
-            $return_message = "Sorry! Was unable to update the volunteer.";
+        // once we have inserted a Volunteer, we can add update the Volunteer, and insert a corresponding Address & Emergency Contact
+        if (!$createFailed) {
+            // get volunteer id
+            $volunteer_id_query = "SELECT id FROM volunteer WHERE email = ?";
+            $results = $db->executeStatement($volunteer_id_query, array($vol_email))->fetchAll();
+            if (isset($results[0])) {
+                $vol_id = $results[0]['id'];
+            }
+
+            $volunteerUpdated = false;
+            $update_volunteer_string = "UPDATE volunteer
+                            SET ";
+
+            if (isset($_POST['skills'])) {
+                $vol_skills = filter_var($_POST['skills'], FILTER_SANITIZE_STRING);
+                $update_volunteer_string = $update_volunteer_string . "skills = '" . $vol_skills . "',";
+                $volunteerUpdated = true;
+            }
+            if (isset($_POST['interests'])) {
+                $vol_interests = filter_var($_POST['interests'], FILTER_SANITIZE_STRING);
+                $update_volunteer_string = $update_volunteer_string . "interests = '" . $vol_interests . "',";
+                $volunteerUpdated = true;
+            }
+            if (isset($_POST['availability'])) {
+                $availability = filter_var($_POST['availability'], FILTER_SANITIZE_STRING);
+                $update_volunteer_string = $update_volunteer_string . "availability = '" . $availability . "',";
+                $volunteerUpdated = true;
+            }
+            if (isset($_POST['find_out_about_us'])) {
+                $vol_find_out_about_us = filter_var($_POST['find_out_about_us'], FILTER_SANITIZE_STRING);
+                $update_volunteer_string = $update_volunteer_string . "find_out_about_us = '" . $vol_find_out_about_us . "',";
+                $volunteerUpdated = true;
+            }
+            if (isset($_POST['email_dist'])) {
+                $email_dist = filter_var($_POST['email_dist'], FILTER_SANITIZE_STRING);
+                if ($email_dist == 'on') {
+                    $email_dist = 1;
+                } else {
+                    $email_dist = 0;
+                }
+                $update_volunteer_string = $update_volunteer_string . "include_email_dist = '" . $email_dist . "',";
+                $volunteerUpdated = true;
+            }
+
+            $update_volunteer_string = substr($update_volunteer_string, 0, -1); // remove extra comma
+            $update_volunteer_string = $update_volunteer_string . "WHERE id = " . $vol_id;
+
+
+            // insert Address
+            $vol_street_one = filter_var($_POST['street_one'], FILTER_SANITIZE_STRING);
+            $vol_street_two = filter_var($_POST['street_two'], FILTER_SANITIZE_STRING);
+            $vol_city = filter_var($_POST['city'], FILTER_SANITIZE_STRING);
+            $vol_state = filter_var($_POST['state'], FILTER_SANITIZE_STRING);
+            $vol_zip = filter_var($_POST['zip'], FILTER_SANITIZE_STRING);
+            // update address fields
+            $addressSet = false;
+            if (isset($vol_street_one) && isset($vol_city) && isset($vol_state) && isset($vol_zip)) {
+                $addressSet = true;
+            }
+            $insert_address_string = "INSERT INTO address (volunteer_id, street_one, street_two, city, state, zip)
+                          VALUES ('" . $vol_id . "','" . $vol_street_one . "','" . $vol_street_two . "','" . $vol_city . "','" . $vol_state . "','" . $vol_zip . "')";
+
+            // insert Emergency Contact
+            $ec_first_name = filter_var($_POST['ec_first_name'], FILTER_SANITIZE_STRING);
+            $ec_last_name = filter_var($_POST['ec_last_name'], FILTER_SANITIZE_STRING);
+            $ec_phone = filter_var($_POST['ec_phone'], FILTER_SANITIZE_STRING);
+
+            // update emergency contact fields
+            $ecSet = false;
+            if (isset($ec_first_name) && isset($ec_last_name) && isset($ec_phone)) {
+                $ecSet = true;
+            }
+            $insert_ec_string = "INSERT INTO emergency_contact (volunteer_id, first_name, last_name, phone)
+                          VALUES ('" . $vol_id . "','" . $ec_first_name . "','" . $ec_last_name . "','" . $ec_phone . "')";
+
+            // update Volunteer table
+            $updatedFailed = false;
+            if ($db->executeStatement($update_volunteer_string, [])) {
+                if ($addressSet) {
+                    // insert to Address table
+                    $output_check = $db->executeStatement($insert_address_string, []);
+                    echo $output_check;
+//                    if ($db->executeStatement($insert_address_string, [])) {
+                    if ($output_check) {
+                    } else {
+                        echo 'here1';
+                        $updatedFailed = true;
+                    }
+                }
+                if ($ecSet) {
+                    // insert to Emergency Contact table
+                    if ($db->executeStatement($insert_ec_string, [])) {
+                        $return_message = "Successfully Updated Volunteer!";
+                    } else {
+                        echo 'here2';
+                        $updatedFailed = true;
+                    }
+                }
+            } else {
+                echo 'here3';
+                $updatedFailed = true;
+            }
+
+            if ($updatedFailed) {
+                $return_message = "Sorry! Was unable to create the volunteer.";
+            }
         }
     }
     if (strrpos($referring_url, "?")) {
--- www/pages/manage-volunteers.php
+++ www/pages/manage-volunteers.php
@@ -506,65 +506,10 @@
 												<label for="city" class="sr-only">City</label>
 												<input class="form-control" id="city" name="city" placeholder="City" tabindex="11" type="text">
 											</div>
-											<div class="form-group col-md-5 col-xs-10">
-												<label for="state" class="sr-only">State</label>
-
-												<select class="form-control" id="state" tabindex="12">
-													<option value="">Please select a state...</option>
-													<option value="AL">Alabama</option>
-													<option value="AK">Alaska</option>
-													<option value="AZ">Arizona</option>
-													<option value="AR">Arkansas</option>
-													<option value="CA">California</option>
-													<option value="CO">Colorado</option>
-													<option value="CT">Connecticut</option>
-													<option value="DE">Delaware</option>
-													<option value="DC">District Of Columbia</option>
-													<option value="FL">Florida</option>
-													<option value="GA">Georgia</option>
-													<option value="HI">Hawaii</option>
-													<option value="ID">Idaho</option>
-													<option value="IL">Illinois</option>
-													<option value="IN">Indiana</option>
-													<option value="IA">Iowa</option>
-													<option value="KS">Kansas</option>
-													<option value="KY">Kentucky</option>
-													<option value="LA">Louisiana</option>
-													<option value="ME">Maine</option>
-													<option value="MD">Maryland</option>
-													<option value="MA">Massachusetts</option>
-													<option value="MI">Michigan</option>
-													<option value="MN">Minnesota</option>
-													<option value="MS">Mississippi</option>
-													<option value="MO">Missouri</option>
-													<option value="MT">Montana</option>
-													<option value="NE">Nebraska</option>
-													<option value="NV">Nevada</option>
-													<option value="NH">New Hampshire</option>
-													<option value="NJ">New Jersey</option>
-													<option value="NM">New Mexico</option>
-													<option value="NY">New York</option>
-													<option value="NC">North Carolina</option>
-													<option value="ND">North Dakota</option>
-													<option value="OH">Ohio</option>
-													<option value="OK">Oklahoma</option>
-													<option value="OR">Oregon</option>
-													<option value="PA">Pennsylvania</option>
-													<option value="RI">Rhode Island</option>
-													<option value="SC">South Carolina</option>
-													<option value="SD">South Dakota</option>
-													<option value="TN">Tennessee</option>
-													<option value="TX">Texas</option>
-													<option value="UT">Utah</option>
-													<option value="VT">Vermont</option>
-													<option value="VA">Virginia</option>
-													<option value="WA">Washington</option>
-													<option value="WV">West Virginia</option>
-													<option value="WI">Wisconsin</option>
-													<option value="WY">Wyoming</option>
-												</select>
-
-											</div>
+                                            <div class="form-group col-md-6 col-xs-12">
+                                                <label for="state" class="sr-only">State</label>
+                                                <input class="form-control" id="state" name="state" placeholder="State" type="text">
+                                            </div>
 										</div>
 										<div class="row">
 											<div class="form-group col-md-10 col-xs-10">
